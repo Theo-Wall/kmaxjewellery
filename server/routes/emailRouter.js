e3beddfe131a03d62f1d627bead2d7847db9af67
Theo-Wall <theo.wall@propra.ca>
2022-06-23 15:36:40 -0600

    set email backend

--- server/routes/emailRouter.js
+++ server/routes/emailRouter.js
@@ -1,9 +1,28 @@
 import express from "express";
+import nodemailer from "nodemailer";
 const router = express.Router();
 
-router.post("/sendEmail", (req, res) => {
+router.post("/sendEmail", async (req, res) => {
   const data = req.body;
-  console.log("data", data);
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp-relay.sendinblue.com",
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: ` "${data.fullName}" <${data.email}>`, // sender address
+      to: "theo.wall11@gmail.com", // list of receivers
+      subject: "Contact KMAXjewellery", // Subject line
+      text: `${data.message}`, // plain text body
+    });
+    res.send(info).status(200);
+  } catch (e) {
+    res.sendStatus(400);
+  }
 });
 
 export default router;

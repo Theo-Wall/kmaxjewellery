f2274b910ffa215767b60dbc53b70e8d50ef6a72
Theo-Wall <theo.wall@propra.ca>
2022-07-03 12:50:39 -0600

    more image muddling

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -25,6 +25,7 @@ export default function ItemModal({ open, setOpen, cat }) {
 
   const [images, setImages] = useState([]);
   const maxNumber = 69;
+
   const onChange = (imageList, addUpdateIndex) => {
     // data for submit
     console.log(imageList, addUpdateIndex);
@@ -34,69 +35,18 @@ export default function ItemModal({ open, setOpen, cat }) {
   const onSubmit = async (data) => {
     console.log("data", data);
     console.log("images", images);
-    // const response = await axios.post("email/sendEmail", data);
-    // console.log("response", response);
   };
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    watch,
+    // formState: { errors },
+    // watch,
   } = useForm();
-
-  //   const [title, description] = watch(["title", "description"]);
-
-  //   console.log("title", title);
 
   const LocalForm = () => {
     return (
       <>
-        <Typography label="title" sx={{ fontWeight: 600 }}>
-          Title:
-        </Typography>
-        <TextField
-          fullWidth
-          id="title"
-          type="text"
-          autoFocus
-          autoComplete="disabled"
-          //   error={errors?.fullName ? true : false}
-          //   helperText={errors?.fullName && "Name Required"}
-          {...register("title", { required: true, maxLength: 100 })}
-          sx={{ mb: "20px" }}
-        />
-        {/* <Typography label="phone" sx={{ fontWeight: 600 }}>
-          Phone Number:
-        </Typography>
-        <TextField
-          fullWidth
-          id="phone"
-          name="phone"
-          type="tel"
-          {...register("phoneNo")}
-          sx={{ mb: "20px" }}
-        /> */}
-
-        <Typography label="description" sx={{ fontWeight: 600 }}>
-          Description:
-        </Typography>
-        <TextField
-          fullWidth
-          id="description"
-          name="description"
-          type="text"
-          multiline
-          rows={10}
-          autoFocus
-          //   error={errors?.message ? true : false}
-          //   helperText={errors?.message && "Message Required"}
-          {...register("description", { required: true })}
-          sx={{ mb: "20px" }}
-        />
-        <Typography label="images" sx={{ fontWeight: 600 }}>
-          Upload Images:
-        </Typography>
         <ImageUploading
           multiple
           value={images}
@@ -136,9 +86,39 @@ export default function ItemModal({ open, setOpen, cat }) {
             </div>
           )}
         </ImageUploading>
-        <Button type="submit" variant="outlined" sx={{ fontWeight: 600 }}>
-          Submit Item
-        </Button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography label="title" sx={{ fontWeight: 600 }}>
+            Title:
+          </Typography>
+          <TextField
+            fullWidth
+            id="title"
+            type="text"
+            autoComplete="disabled"
+            {...register("title", { required: true, maxLength: 100 })}
+            sx={{ mb: "20px" }}
+          />
+
+          <Typography label="description" sx={{ fontWeight: 600 }}>
+            Description:
+          </Typography>
+          <TextField
+            fullWidth
+            id="description"
+            name="description"
+            type="text"
+            multiline
+            rows={10}
+            {...register("description", { required: true })}
+            sx={{ mb: "20px" }}
+          />
+          <Typography label="images" sx={{ fontWeight: 600 }}>
+            Upload Images:
+          </Typography>
+          <Button type="submit" variant="outlined" sx={{ fontWeight: 600 }}>
+            Submit Item
+          </Button>
+        </form>
       </>
     );
   };
@@ -155,12 +135,11 @@ export default function ItemModal({ open, setOpen, cat }) {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Add new item in {cat}
           </Typography>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <LocalForm />
-            {/* <Box>
+
+          <LocalForm />
+          {/* <Box>
               <ItemCard title={title} description={description} />
             </Box> */}
-          </form>
         </Box>
       </Modal>
     </div>

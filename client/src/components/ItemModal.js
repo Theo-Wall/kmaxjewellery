a9825c950123ce09655f14a5ae097e91768f21a7
Theo-Wall <theo.wall@propra.ca>
2022-07-03 11:32:50 -0600

    setting up image uploader

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -2,8 +2,10 @@ import { TextField, Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import ItemCard from "../components/ItemCard";
+import ImageUploading from "react-images-uploading";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import "./ItemModal.css";
 
 const style = {
   position: "absolute",
@@ -21,8 +23,17 @@ const style = {
 export default function ItemModal({ open, setOpen, cat }) {
   const handleClose = () => setOpen(false);
 
+  const [images, setImages] = useState([]);
+  const maxNumber = 69;
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
   const onSubmit = async (data) => {
     console.log("data", data);
+    console.log("images", images);
     // const response = await axios.post("email/sendEmail", data);
     // console.log("response", response);
   };
@@ -83,8 +94,50 @@ export default function ItemModal({ open, setOpen, cat }) {
           {...register("description", { required: true })}
           sx={{ mb: "20px" }}
         />
+        <Typography label="images" sx={{ fontWeight: 600 }}>
+          Upload Images:
+        </Typography>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              <button
+                style={isDragging ? { color: "red" } : null}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button onClick={onImageRemoveAll}>Remove all images</button>
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image.data_url} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
         <Button type="submit" variant="outlined" sx={{ fontWeight: 600 }}>
-          Send Message
+          Submit Item
         </Button>
       </>
     );

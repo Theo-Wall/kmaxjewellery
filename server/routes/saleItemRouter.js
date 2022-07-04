d9df20d18f6974fa1e4fbafa305d09eb303d860f
Theo-Wall <theo.wall@propra.ca>
2022-07-04 16:24:23 -0600

    finished upload endpoint

--- server/routes/saleItemRouter.js
+++ server/routes/saleItemRouter.js
@@ -6,37 +6,25 @@ import upload from "../utils/multer.js";
 const router = express.Router();
 
 router.post("/upload", upload.single("image"), async (req, res) => {
-  //   const result = await cloudinary.uploader.upload(req.file.path);
-  //   console.log("result", result);
+  const result = await cloudinary.uploader.upload(req.file.path);
+  console.log("result", result);
 
   const itemData = JSON.parse(req.body.formData);
-
-  console.log("itemData", itemData);
 
   itemData.images = ["testing url"];
 
   const card = {
     ...itemData,
-    images: ["testing url"],
+    images: [result.secure_url],
     createdZ: new Date(),
     lastEditedZ: new Date(),
   };
-
-  console.log("card", card);
 
   const newCard = await new Card(card);
 
   const newSavedCard = await newCard.save();
 
-  console.log("newSavedCard", newSavedCard);
-
-  //   // saves "newData" to the saleItems collection in database
-  //   const newItem = await newData.save();
-
-  //   // await findUserAndUpdate(postItem.vendorId, updatedSaleItems)
-
-  //   // sends result from database back to front end
-  //   res.json(newItem);
+  res.send(newSavedCard).status(200);
 });
 
 export default router;
